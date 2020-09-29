4d2c0207d1211072a8d4f7162b7719c4b7be4e93
Daniel Aguirre <daniel.aguirre7478@alumnos.udg.mx>
2020-09-29 12:53:49 -0500

    Actividad 7 funciona

--- main.cpp
+++ main.cpp
@@ -3,7 +3,49 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
-
+    string tipo;
+    getline(cin,tipo);
+    if (tipo == "int")
+    {
+        cout<<sizeof(int);
+    }
+    else
+    {
+        if(tipo == "char")
+        {
+            cout<<sizeof(char);
+        }    
+        else
+        {
+            if(tipo == "short")
+            {
+                cout<<sizeof(short);
+            }
+            else
+            {
+                if(tipo == "float")
+                {
+                    cout<<sizeof(float);
+                }
+                else
+                {
+                    if(tipo == "double")
+                    {
+                        cout<<sizeof(double);
+                    }
+                    else
+                    {
+                        if(tipo == "long")
+                        {
+                            cout<<sizeof(long);
+                        }
+                    }
+                    
+                }
+                
+            }
+            
+        }
+    }
     return 0;
 }
